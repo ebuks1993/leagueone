71756957813b29763cece95b2d0a2596eb7e9d06
Chukwuka Ebuka <chukwuka932@gmail.com>
2023-05-13 15:00:26 +0100

    first commit

--- league/views.py
+++ league/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import gameSerializer
 from .models import games
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
 class gameViewset(ModelViewSet):
     queryset=games.objects.all()
     serializer_class=gameSerializer
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=['Date']

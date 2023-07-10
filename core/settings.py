8883faedd842e1622ee058c3888fec66e4d5c79d
Chukwuka Ebuka <chukwuka932@gmail.com>
2023-07-10 11:16:43 +0100

    first commit

--- core/settings.py
+++ core/settings.py
@@ -151,12 +151,28 @@ else:
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Email
+ 
+
+passy=os.environ.get('EMAIL_HOST_PASSWORD')
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIT_HOST')
-EMAIT_PORT = os.environ.get('EMAIT_PORT')
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIT_PORT = 587
+EMAIL_USE_TLS =True
+# EMAIL_USE_SSL =True
+EMAIL_HOST_USER ="apikey"
+EMAIL_HOST_PASSWORD =passy
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIT_PORT = 587
+# EMAIL_USE_TLS =True
+# # EMAIL_USE_SSL =True
+# EMAIL_HOST_USER ='chukwuka932@gmail.com'
+# EMAIL_HOST_PASSWORD ="chuk2468"
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

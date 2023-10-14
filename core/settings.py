418d338a5e82e1cb6ced8684ead77419f9e333f8
Chukwuka Ebuka <chukwuka932@gmail.com>
2023-10-14 19:24:56 +0100

    first commit

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&b%*34il=by7tn#oidjl@#4#2^(m08dnov1it&4_$48^on@p&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
